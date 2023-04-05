4f4d6fae79d7ac52fc456dc95376fd89f7092e1d
danicaon <danica_escot@hotmail.com>
2023-04-04 22:12:27 -0500

    adding engineer Imput on index.js

--- index.js
+++ index.js
@@ -1,10 +1,66 @@
+
+// Dev
 const fs = require('fs');
 const path = require('path');
 const inquirer = require('inquirer');
 const Engineer = require('./lib/engineer');
 const Manager = require('./lib/manager');
 const Intern = require('./lib/intern');
-const generateHtml = require('./utils/generateHtml');
+const generateHtml = require('./lib/generateHTML');
+
+// engineer questions
+const engineerInput = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "Please input the engineer's name: ",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('ERROR! Please try again!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "Please input the engineer's email: ",
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log('ERROR! Please try again!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'gitHub',
+            message: "Please input the engineer's gitHub: ",
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                } else {
+                    console.log('ERROR! Please try again!');
+                    return false;
+                }
+            }
+        },
+    ]).then(answers => {
+        const engineer = new Engineer(answers.name, answers.id, answers.email, answers.gitHub);
+        team.push(engineer);
+        teamPrompt();
+    })
+}
+// manager questions
+
+//intern questions
+
+
 
 
 
