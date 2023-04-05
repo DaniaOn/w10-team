282fee21a5d379e447e246826a4f85b496833b7b
danicaon <danica_escot@hotmail.com>
2023-04-04 22:00:09 -0500

    adding CSS branch

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ const inquirer = require('inquirer');
 const Engineer = require('./lib/engineer');
 const Manager = require('./lib/manager');
 const Intern = require('./lib/intern');
+const generateHtml = require('./utils/generateHtml');
+
 
 
